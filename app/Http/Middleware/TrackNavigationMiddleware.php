167b9fd3a704da38613acc1478c2a89f5d3969c5
yamaryosan <canda3267@gmail.com>
2023-12-14 22:41:23 +0900

    [add] セッション履歴のうち古いものを削除する機能を実装

--- app/Http/Middleware/TrackNavigationMiddleware.php
+++ app/Http/Middleware/TrackNavigationMiddleware.php
@@ -8,6 +8,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 class TrackNavigationMiddleware
 {
+    // ナビゲーション履歴の最大数
+    const MAX_HISTORY = 10;
+
     /**
      * Handle an incoming request.
      *
@@ -18,17 +21,25 @@ class TrackNavigationMiddleware
         // 現在のルート名を取得
         $currentRoute = $request->path();
 
+        // セッションを定義
+        $session = $request->session();
+
         // セッションにナビゲーション履歴があれば取得、なければ初期化
-        if(!$request->session()->get('navigation_history')){
-            $request->session()->put('navigation_history', []);
+        if(!$session->get('navigation_history')){
+            $session->put('navigation_history', []);
         }
 
-        // セッションにナビゲーション履歴を追加
-        $history = $request->session()->get('navigation_history');
+        // ナビゲーション履歴に現在のルート名を追加
+        $history = $session->get('navigation_history');
         $history[] = $currentRoute;
 
+        // 履歴が一定以上あれば古いものから削除
+        if(count($history) > self::MAX_HISTORY){
+            array_shift($history);
+        }
+
         // セッションにナビゲーション履歴を保存
-        $request->session()->put('navigation_history', $history);
+        $session->put('navigation_history', $history);
 
         return $next($request);
     }
