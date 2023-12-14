325aa301e130ff1fceb0946470f7e2a7ce9734f3
yamaryosan <canda3267@gmail.com>
2023-12-14 22:06:14 +0900

    [add] ページ遷移パターンチェック用ミドルウェア作成

--- app/Http/Middleware/CheckNavigationPattern.php
+++ app/Http/Middleware/CheckNavigationPattern.php
@@ -15,6 +15,26 @@ class CheckNavigationPattern
      */
     public function handle(Request $request, Closure $next): Response
     {
+        // 履歴チェック
+        $history = $request->session()->get('navigation_history') ?: [];
+
+        // 履歴が特定のパターンに一致するかチェック
+        if($this->isValidPattern($history)) {
+            // 一致したら自宅閲覧モードのランディングページに遷移
+            return redirect()->route('s');
+        }
+
         return $next($request);
+    }
+
+    // パターンに一致するかチェック
+    // 一致する場合はtrueを返す
+    protected function isValidPattern(array $history)
+    {
+        $pattern = ["index", "s"];
+        if (count($history) < count($pattern)) {
+            return false;
+        }
+        return $history === $pattern;
     }
 }
