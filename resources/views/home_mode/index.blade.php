4344d5313be7456bc7bb73f8b53a1c7160cb1d53
yamaryosan <canda3267@gmail.com>
2023-12-15 20:09:31 +0900

    [add] ランディングページレイアウト

--- resources/views/home_mode/index.blade.php
+++ resources/views/home_mode/index.blade.php
@@ -1,5 +1,19 @@
+@extends('home_mode.app')
+
+@section('content')
+
 <h2>自宅閲覧モード</h2>
 
 @component('components.search_window')
 
 @endcomponent
+
+<h2>項目新規作成</h2>
+
+<h2>ランダム表示</h2>
+
+<h2>統計情報</h2>
+
+<h2>インポート・エクスポート</h2>
+
+@endsection
