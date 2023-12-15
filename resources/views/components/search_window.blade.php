a4c07911d1b325383f1954f97b50b1c0cb85b560
yamaryosan <canda3267@gmail.com>
2023-12-15 21:15:49 +0900

    [add] 検索ウィンドウの検索結果表示機能実装

--- resources/views/components/search_window.blade.php
+++ resources/views/components/search_window.blade.php
@@ -1,19 +1,19 @@
 <!-- 検索ウィンドウのコンポーネント -->
 
 <div class="search_window">
-    <div class="search_window_inner">
-        <div class="search_window_inner_title">
-            <h2>検索</h2>
+    <form action="{{ route('search') }}" method="GET">
+        <div class="search_window_inner_form_input">
+            <input type="text" name="keyword" placeholder="キーワードを入力">
         </div>
-        <div class="search_window_inner_form">
-            <form action="{{ route('search') }}" method="GET">
-                <div class="search_window_inner_form_input">
-                    <input type="text" name="keyword" placeholder="キーワードを入力">
-                </div>
-                <div class="search_window_inner_form_submit">
-                    <input type="submit" value="検索">
-                </div>
-            </form>
+        <div class="search_window_inner_from_select">
+            <select name="target">
+                <option value="0">Title</option>
+                <option value="1">Detail</option>
+                <option value="2">Both</option>
+            </select>
         </div>
-    </div>
+        <div class="search_window_inner_form_submit">
+            <input type="submit" value="検索">
+        </div>
+    </form>
 </div>
