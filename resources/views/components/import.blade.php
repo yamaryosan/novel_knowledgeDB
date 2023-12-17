df193e59e8f60ea12c7e3edf8fa8e4a300097119
yamaryosan <canda3267@gmail.com>
2023-12-17 10:29:17 +0900

    [update] クラス名変更

--- resources/views/components/import.blade.php
+++ resources/views/components/import.blade.php
@@ -1,9 +1,12 @@
 <!-- インポート用のフォーム -->
 
-<div class="import_export">
-    <form class="import" action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
+<div class="import">
+    <form class="import_form" action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <input class="import_file_part" type="file" name="file">
-        <input class="import_btn" type="submit" value="インポート">
+        <div class="drop_area">
+            <div id="importFileIcon" role="button" tabindex="0">ファイルを選択</div>
+            <input id="fileInput" class="import_file_part" type="file" name="file">
+        </div>
+        <input class="submit_btn" type="submit" value="アップロード">
     </form>
 </div>
