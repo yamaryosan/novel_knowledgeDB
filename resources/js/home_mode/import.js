78d462cecd4541cf48e2b5935ccdc69596ed6a9d
yamaryosan <canda3267@gmail.com>
2023-12-17 11:21:57 +0900

    [add] ドロップ時のスタイリング (背景色変更)

--- resources/js/home_mode/import.js
+++ resources/js/home_mode/import.js
@@ -1,8 +1,16 @@
+const dropAreaElement = document.querySelector('.drop_area');
 const fileIconElement = document.getElementById('importFileIcon');
 const inputElement = document.getElementById('fileInput');
 
+// ファイル選択時の処理
 fileIconElement.addEventListener('click', function() {
     inputElement.click();
+
+    inputElement.addEventListener('change', ()=> {
+        const fileName = inputElement.files[0].name;
+        dropAreaElement.innerText = `${fileName}を選択中`;
+        dropAreaElement.classList.add('drop');
+    });
 });
 
 fileIconElement.addEventListener('keydown', (event)=> {
@@ -24,9 +32,11 @@ fileIconElement.addEventListener('dragleave', ()=> {
     fileIconElement.classList.remove('dragover');
 });
 
+// ドロップした時の処理
 fileIconElement.addEventListener('drop', (event)=> {
     event.preventDefault();
     fileIconElement.classList.remove('dragover');
+    dropAreaElement.classList.add('drop');
 
     // ファイルを取得
     const files = event.dataTransfer.files;
@@ -40,4 +50,8 @@ fileIconElement.addEventListener('drop', (event)=> {
 
     // input要素にファイルをセット
     inputElement.files = dataTransfer.files;
+
+    // ファイル名を表示
+    const fileName = files[0].name;
+    dropAreaElement.innerText = fileName;
 });
