bd5de3dd102ec2e81836791a57aa9cef837e7777
yamaryosan <canda3267@gmail.com>
2023-12-18 20:58:12 +0900

    [add] タイトル、概要、詳細の文字列追加

--- resources/views/components/new_addition.blade.php
+++ resources/views/components/new_addition.blade.php
@@ -2,8 +2,11 @@
 
 <form action="{{ route('store') }}" class="new_addition_form" method="POST">
     @csrf
+    <p>タイトル</p>
     <input type="text" class="title_input" name="title" required>
+    <p>概要</p>
     <input type="text" class="summary_input" name="summary" required>
+    <p>詳細</p>
     <input type="text" class="detail_input" name="detail" required>
-    <input type="submit" value="追加">
+    <input type="submit" value="追加" class="submit_btn">
 </form>
