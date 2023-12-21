b36f283bfb1bb6ff5d45d2b2149664d8cc7cad21
yamaryosan <canda3267@gmail.com>
2023-12-21 21:38:01 +0900

    [fix] ガード節の修正

--- resources/js/home_mode/textarea_autosizing.js
+++ resources/js/home_mode/textarea_autosizing.js
@@ -1,10 +1,12 @@
 /* textareaの高さを自動調整する */
 
 function autosizing(className) {
-    if (className === undefined) {
-        console.error('className is undefined');
-    }
     const textarea = document.querySelector(`.${className}`);
+
+    if (!textarea) {
+        console.error(`textarea_autosizing.js: ${className} is not found.`);
+    }
+
     textarea.addEventListener('input', function() {
         this.style.height = 'auto';
         this.style.height = (this.scrollHeight) + 'px';
