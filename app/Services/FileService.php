303d83d1d840c926921380fc66713fbd2a141a1c
yamaryosan <canda3267@gmail.com>
2023-12-24 01:21:30 +0900

    [add] 新タイプ、旧タイプ各雑学のインポート処理を追加

--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -28,8 +28,8 @@ class FileService
 
             // ファイルをアップロード
             Storage::putFileAs($this->path, $file, $filename);
-            return true;
         }
+        return true;
     }
 
     // テキストファイルかどうかを判定
@@ -50,30 +50,66 @@ class FileService
     public function read(): array
     {
         // ファイルを読み込み、配列に格納
+        $files = Storage::files($this->path);
         $trivia = [];
-        foreach (Storage::files($this->path) as $file) {
-            $fileWholeContent = Storage::get($file);
-            $eachContent = $this->splitByNewline($fileWholeContent);
-            // 項目ごとにタイトルと総論に分割
-            foreach ($eachContent as $content) {
-                $content = $this->splitByDoubleEllipsis($content);
-                // タイトルと総論を取得
-                $title = $content[0];
-                $summary = 'EMPTY';
-                $detail = $content[1];
-                $trivia[] = ['title' => $title, 'summary' => $summary, 'detail' => $detail];
+        foreach ($files as $file) {
+            $wholeContent = Storage::get($file);
+            if ($this->isOldTypeFile($wholeContent)) {
+                $trivia[] = $this->readOldType($wholeContent);
+            } else {
+                $trivia[] = $this->readNewType($wholeContent);
             }
+            // ファイルを削除
+            $this->delete($file);
         }
-        // ファイルを削除
-        $this->delete($file);
-
-        return $trivia;
+        // 2次元配列の項目を1次元に変換
+        $flattenTrivia = $this->flatten($trivia);
+        return $flattenTrivia;
     }
 
     // ファイルを削除
     private function delete($file): void
     {
         Storage::delete($file);
+    }
+
+    // 旧タイプの項目のファイルかどうかを判定
+    private function isOldTypeFile(string $content): bool
+    {
+        // 3点リーダーが含まれていれば旧タイプ
+        if (gettype(strpos($content, '……')) === 'integer') {
+            return true;
+        }
+        return false;
+    }
+
+    // 旧タイプの項目を読み込む
+    private function readOldType(string $wholeContent): array
+    {
+        // 3点リーダーで文字列を分割
+        $contentArray = $this->splitByNewline($wholeContent);
+        $trivia = [];
+        // 項目ごとに分割し、タイトルと詳細を取得
+        foreach ($contentArray as $content){
+            $trivium = $this->splitByDoubleEllipsis($content);
+            $title = $trivium[0];
+            $summary = 'EMPTY';
+            $detail = $trivium[1];
+            $trivia[] = ['title' => $title, 'summary' => $summary, 'detail' => $detail];
+        }
+        return $trivia;
+    }
+
+    // 新タイプの項目を読み込む
+    private function readNewType(string $content): array
+    {
+        // 正規表現を使用して各セクションを抽出
+        preg_match("/【タイトル】\n(.+)\n\n【総論】\n(.+)\n\n【本文】\n(.+)/", $content, $matches);
+        $title = $matches[1];
+        $summary = $matches[2];
+        $detail = $matches[3];
+        $trivia[] = ['title' => $title, 'summary' => $summary, 'detail' => $detail];
+        return $trivia;
     }
 
     // 改行コードでファイルの内容を分割
@@ -88,5 +124,12 @@ class FileService
     {
         $content = explode('……', $content);
         return $content;
+    }
+
+    // 2次元配列の項目を1次元に変換
+    private function flatten(array $trivia): array
+    {
+        $trivia = array_merge(...$trivia);
+        return $trivia;
     }
 }
