d420a8406574dc2aa925d49c28ef8cf4c748df0b
yamaryosan <canda3267@gmail.com>
2023-12-25 08:01:58 +0900

    [update] クラス名変更

--- resources/views/components/article_item.blade.php
+++ resources/views/components/article_item.blade.php
@@ -1,2 +1,19 @@
 <a href="{{ route('show', ['id' => $trivium->id]) }}">
+    <div class="trivium">
+        <div class="item_title">
+            <h3>{{ $trivium->title }}</h3>
+        </div>
+        <div class="item_summary">
+            @if ($trivium->summary !== 'EMPTY')
+                <p>{{ $trivium->summary }}</p>
+            @endif
+        </div>
+        <div class="item_detail">
+            <p>{{ $trivium->detail }}</p>
+        </div>
+        <div class="item_footer">
+            <p>作成日：{{ $trivium->created_at }}</p>
+            <p>更新日：{{ $trivium->updated_at }}</p>
+        </div>
+    </div>
 </a>
