3c0307d8bbdf8abdb025aab4d729def736a393da
yamaryosan <canda3267@gmail.com>
2023-12-24 11:39:32 +0900

    [add] インポート成功メッセージコンポーネント

---
+++ resources/views/components/import_succeed.blade.php
@@ -0,0 +1,10 @@
+<!-- インポート成功時のメッセージ -->
+
+@if (session('flash_succeed_message'))
+    <div class="import_succeed_container">
+        <div class="message">
+            <img src="{{ asset('images/check_icon.png') }}" alt="succeed" class="import_succeed_img">
+            <p>{{ session('flash_succeed_message') }}</p>
+        </div>
+    </div>
+@endif
