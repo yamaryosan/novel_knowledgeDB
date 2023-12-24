ac5ccc5ac573582fbecf7f96add4187dc2334271
yamaryosan <canda3267@gmail.com>
2023-12-25 08:06:42 +0900

    [add] html階層を追加

--- resources/views/home_mode/result.blade.php
+++ resources/views/home_mode/result.blade.php
@@ -1,7 +1,24 @@
-<p>キーワード：{{ $keyword }} (対象: {{ $target }})</p>
-<p> {{ $trivia->count() }}件ヒット </p>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>新規追加</title>
+        @vite([
+            'resources/css/app.css',
+            'resources/css/home_mode/home_mode.css',
+            'resources/css/home_mode/result.css',
+            'resources/css/home_mode/article_item.css',
+            ])
+    </head>
 
-@foreach($trivia as $trivium)
-    @component('components.article_item', ['trivium' => $trivium])
-    @endcomponent
-@endforeach
+    <body>
+        <main>
+            <p>キーワード：{{ $keyword }} (対象: {{ $target }})</p>
+            <p> {{ $trivia->count() }}件ヒット </p>
+
+            @foreach($trivia as $trivium)
+                @component('components.article_item', ['trivium' => $trivium])
+                @endcomponent
+            @endforeach
+        </main>
+</html>
