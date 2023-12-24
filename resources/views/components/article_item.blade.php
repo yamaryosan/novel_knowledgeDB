51eb544ea8901d9782213bcd0f6e5de23d463c7a
yamaryosan <canda3267@gmail.com>
2023-12-25 08:01:36 +0900

    [add] show用リンクを追加

--- resources/views/components/article_item.blade.php
+++ resources/views/components/article_item.blade.php
@@ -1,17 +1,2 @@
-<div class="trivium">
-    <div class="item_title">
-        <h3>{{ $trivium->title }}</h3>
-    </div>
-    <div class="item_summary">
-        @if ($trivium->summary !== 'EMPTY')
-            <p>{{ $trivium->summary }}</p>
-        @endif
-    </div>
-    <div class="item_detail">
-        <p>{{ $trivium->detail }}</p>
-    </div>
-    <div class="item_footer">
-        <p>作成日：{{ $trivium->created_at }}</p>
-        <p>更新日：{{ $trivium->updated_at }}</p>
-    </div>
-</div>
+<a href="{{ route('show', ['id' => $trivium->id]) }}">
+</a>
