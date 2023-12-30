7012e2c38927e3469c25a64d5dc0afe5da5ceb63
yamaryosan <canda3267@gmail.com>
2023-12-30 22:45:56 +0900

    [add] 編集ボタン追加

--- resources/views/components/article_item.blade.php
+++ resources/views/components/article_item.blade.php
@@ -3,6 +3,9 @@
         <div class="item_title">
             <h3>{{ $trivium->title }}</h3>
         </div>
+        <div class="edit_btn_container">
+            <a href="{{ route('edit', ['id' => $trivium->id]) }}" class="edit_btn">編集</a>
+        </div>
         <div class="item_summary">
             @if ($trivium->summary === 'EMPTY')
                 <p>{{ Str::limit($trivium->detail, 100) }}</p>
