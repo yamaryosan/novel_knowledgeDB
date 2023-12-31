6a419fb2acfa71b6a011d3f93078e84f8ff97a92
yamaryosan <canda3267@gmail.com>
2024-01-01 01:03:31 +0900

    [update] 入力に応じてデフォルトで高さを自動調整

--- resources/js/home_mode/textarea_autosizing.js
+++ resources/js/home_mode/textarea_autosizing.js
@@ -7,6 +7,11 @@ function autosizing(className) {
         console.error(`textarea_autosizing.js: ${className} is not found.`);
     }
 
+    // デフォルトで高さを自動調整
+    textarea.style.height = 'auto';
+    textarea.style.height = (textarea.scrollHeight) + 'px';
+
+    // 入力があったら高さを自動調整
     textarea.addEventListener('input', function() {
         this.style.height = 'auto';
         this.style.height = (this.scrollHeight) + 'px';
