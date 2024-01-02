493e2e58c2581df52dc1438c8301b62b88243cdb
yamaryosan <canda3267@gmail.com>
2024-01-03 00:40:57 +0900

    [fix] エラー解消

--- resources/js/home_mode/error_message_hide.js
+++ resources/js/home_mode/error_message_hide.js
@@ -3,5 +3,8 @@ const errorContainer = document.querySelector('.error_container');
 
 // 画面のどこかがクリックされたらエラーを非表示にする
 body.addEventListener('click', function() {
+    if (errorContainer === null) {
+        return;
+    }
     errorContainer.classList.add('hidden');
 });
