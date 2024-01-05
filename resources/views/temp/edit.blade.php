54dd3ac699c45798cea31c2f683cf21267a40754
yamaryosan <canda3267@gmail.com>
2024-01-05 23:46:53 +0900

    [add] 編集用フォーム

---
+++ resources/views/temp/edit.blade.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>一時保存項目編集</title>
+        @vite([
+            'resources/css/app.css',
+            'resources/css/home_mode/home_mode.css',
+            'resources/css/home_mode/edit_form.css',
+            'resources/js/home_mode/edit_form.js',
+            'resources/js/home_mode/exit_prevention.js',
+            'resources/js/temp/temp_edit_form.js'
+            ])
+    </head>
+    <body>
+        <main>
+            @component('components.temp_edit_form',
+            ['id'=>$tempTrivium->id, 'title'=>$tempTrivium->title, 'summary'=>$tempTrivium->summary, 'detail'=>$tempTrivium->detail])
+            @endcomponent
+        </main>
+    </body>
+</html>
