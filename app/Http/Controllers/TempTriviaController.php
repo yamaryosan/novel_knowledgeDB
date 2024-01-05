67db36403d942dba55dd5a44a97ff65fcf9f306e
yamaryosan <canda3267@gmail.com>
2024-01-05 20:59:40 +0900

    [add] アクションメソッド追加

--- app/Http/Controllers/TempTriviaController.php
+++ app/Http/Controllers/TempTriviaController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Models\TempTrivia;
+use App\Models\TempTrivium;
 
 class TempTriviaController extends Controller
 {
-    //
+    public function index()
+    {
+        $tempTrivia = TempTrivium::all();
+        return view('home_mode.index', compact('tempTrivia'));
+    }
 }
