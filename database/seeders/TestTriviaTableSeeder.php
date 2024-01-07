0341c139bb6943eb4ccac1b86e8ecae3dd4ea186
yamaryosan <canda3267@gmail.com>
2024-01-07 23:37:36 +0900

    [add] シーディング

---
+++ database/seeders/TestTriviaTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+
+class TestTriviaTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('test_trivia')->insert([
+            'title' => 'test_title',
+            'summary' => 'test_summary',
+            'detail' => 'test_detail',
+            'created_at' => '2024-01-07 14:19:08',
+            'updated_at' => '2024-01-07 14:19:08',
+        ]);
+    }
+}
