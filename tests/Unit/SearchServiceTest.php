f3357ebb1570701e9aca1492008795551e43e234
yamaryosan <canda3267@gmail.com>
2024-01-08 21:03:12 +0900

    [add] SearchServiceのためのメソッド追加

--- tests/Unit/SearchServiceTest.php
+++ tests/Unit/SearchServiceTest.php
@@ -3,14 +3,26 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use App\Services\SearchService;
 
 class SearchServiceTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
+    public function testSearchServiceThrowsExceptionWhenKeywordIsEmpty()
     {
-        $this->assertTrue(true);
+        $this->expectException(\Exception::class);
+        $searchService = new SearchService("", "title");
+    }
+
+    public function testSearchServiceThrowsExceptionWhenKeywordIsTooLong()
+    {
+        $this->expectException(\Exception::class);
+        $tooLongKeyword = str_repeat("a", env('MAX_SEARCH_KEYWORD_LENGTH') + 1);
+        $searchService = new SearchService($tooLongKeyword, "title");
+    }
+
+    public function testSearchServiceThrowsExceptionWhenTargetIsInvalid()
+    {
+        $this->expectException(\Exception::class);
+        $searchService = new SearchService("keyword", "invalid");
     }
 }
