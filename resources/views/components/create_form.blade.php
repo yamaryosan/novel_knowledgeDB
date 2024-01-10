e87a08e8c16c88781aff51f34cbcbc5048e34ebb
yamaryosan <canda3267@gmail.com>
2024-01-11 00:07:26 +0900

    [update] プレビューからの値を表示

--- resources/views/components/create_form.blade.php
+++ resources/views/components/create_form.blade.php
@@ -4,15 +4,15 @@
     @csrf
     <div class="title_container">
         <p>タイトル</p>
-        <input type="text" class="title_input" name="title" required>
+        <input type="text" class="title_input" name="title" value="{{ $title }}" required>
     </div>
     <div class="summary_container">
         <p>概要</p>
-        <textarea name="summary" class="summary_input" cols="20" rows="5" required></textarea>
+        <textarea name="summary" class="summary_input" cols="20" rows="5" required>{{ $summary }}</textarea>
     </div>
     <div class="detail_container">
         <p>詳細</p>
-        <textarea name="detail" class="detail_input" cols="20" rows="5" required></textarea>
+        <textarea name="detail" class="detail_input" cols="20" rows="5" required>{{ $detail }}</textarea>
     </div>
     @component('components.button')
         @slot('name', 'save_btn')
