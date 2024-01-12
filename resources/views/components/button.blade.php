48257e2e69eb660728172f01b37b3ec7f0973886
yamaryosan <canda3267@gmail.com>
2024-01-13 00:41:19 +0900

    [add] データ属性を指定できるよう追加

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,5 +1,8 @@
 <input type="button"
 class="{{ $name }} {{ $image }} {{ $color }}"
+@isset($data_name)
+data-{{ $data_name }}="{{ $data_value }}"
+@endisset
 @isset($link) onClick="location.href='{{ $link }}'"
 @endisset
 >
