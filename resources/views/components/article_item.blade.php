dd372177305e4fb755489f7355cde504f2419257
yamaryosan <canda3267@gmail.com>
2024-01-12 23:12:13 +0900

    [update] 不要なdiv要素を削除

--- resources/views/components/article_item.blade.php
+++ resources/views/components/article_item.blade.php
@@ -1,19 +1,13 @@
 <div class="article_item_container">
-    <a href="{{ route('show', ['id' => $trivium->id]) }}" class="trivium_link">
+    <a class="trivium_link" href="{{ route('show', ['id' => $trivium->id]) }}">
         <div class="trivium_unit">
-            <div class="item_title">
-                <h3>{{ $trivium->title }}</h3>
-            </div>
-            <div class="item_summary">
-                @if ($trivium->summary === '')
-                    <p>{{ Str::limit($trivium->detail, 100) }}</p>
-                @else
-                    <p>{{ Str::limit($trivium->summary, 300) }}</p>
-                @endif
-            </div>
-            <div class="item_footer">
-                <p>{{ $trivium->updated_at }}</p>
-            </div>
+            <h3>{{ $trivium->title }}</h3>
+            @if ($trivium->summary === '')
+                <p>{{ Str::limit($trivium->detail, 100) }}</p>
+            @else
+                <p>{{ Str::limit($trivium->summary, 300) }}</p>
+            @endif
+            <p>{{ $trivium->updated_at }}</p>
         </div>
     </a>
     @component('components.edit_button')
