58b65bf33dfa2ae94d2fd1df70a4bfb6ba48f66d
yamaryosan <canda3267@gmail.com>
2024-01-13 17:23:18 +0900

    [add] 論理削除ルーティング

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PseudoPageController;
 use App\Http\Controllers\WorkspaceModeController;
 use App\Http\Controllers\HomeModeController;
 use App\Http\Controllers\TempTriviaController;
+use App\Http\Controllers\SoftDeleteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -41,7 +42,6 @@ Route::post('/store', [HomeModeController::class, 'store'])->name('store');
 Route::get('/show/{id}', [HomeModeController::class, 'show'])->name('show');
 Route::get('/edit/{id}', [HomeModeController::class, 'edit'])->name('edit');
 Route::post('/update/{id}', [HomeModeController::class, 'update'])->name('update');
-Route::get('/delete/{id}', [HomeModeController::class, 'delete'])->name('delete');
 Route::post('/preview/{id?}', [HomeModeController::class, 'preview'])->name('preview');
 Route::post('/back', [HomeModeController::class, 'back'])->name('back');
 Route::get('/all_delete', [HomeModeController::class, 'all_delete'])->name('all_delete');
@@ -53,3 +53,8 @@ Route::get('/temp/delete/{id}', [TempTriviaController::class, 'delete'])->name('
 Route::post('/temp/preview/{id}', [TempTriviaController::class, 'preview'])->name('temp_preview');
 Route::post('/temp/back', [TempTriviaController::class, 'back'])->name('temp_back');
 Route::post('/temp/migrate/{id}', [TempTriviaController::class, 'migrate'])->name('temp_migrate');
+Route::get('/soft_delete/index', [SoftDeleteController::class, 'index'])->name('soft_delete_index');
+Route::get('/soft_delete/store/{id}', [SoftDeleteController::class, 'soft_delete'])->name('soft_delete');
+Route::get('/soft_delete/preview/{id}', [SoftDeleteController::class, 'preview'])->name('soft_delete_preview');
+Route::post('/soft_delete/restore/{id}', [SoftDeleteController::class, 'restore'])->name('soft_delete_restore');
+Route::get('/delete/{id}', [SoftDeleteController::class, 'delete'])->name('delete');
