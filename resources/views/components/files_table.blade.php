1890edeb773eb27a79bf6ace2d410895b15e459d
yamaryosan <canda3267@gmail.com>
2024-01-15 00:55:19 +0900

    [add] ファイル一覧表示用テンプレートファイル

---
+++ resources/views/components/files_table.blade.php
@@ -0,0 +1,27 @@
+<table>
+    <tbody>
+        @if (count($files) === 0)
+        <tr>
+            <td>ファイルはありません</td>
+        </tr>
+        @else
+            @foreach ($files as $file)
+            <tr>
+                <td>{{ $file['filename'] }}</td>
+                <td>{{ $file['uploaded_at'] }}</td>
+                <td>{{ $file['article_count'] }}個</td>
+                <td>
+                    <a href="/storage/export/{{ $file['filename'] }}" download="{{ $file['filename'] }}">
+                        <img src="{{ asset('images/download.png') }}" alt="download">
+                    </a>
+                </td>
+                <td>
+                    <a href="{{ route('export_delete', ['filename' => $file['filename']]) }}">
+                        <img src="{{ asset('images/trashbox.png') }}" alt="trashbox">
+                    </a>
+                </td>
+            </tr>
+            @endforeach
+        @endif
+    </tbody>
+</table>
