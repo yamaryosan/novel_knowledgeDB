2e45003c9f91eb6f694cdc95a8fafff742c052c3
yamaryosan <canda3267@gmail.com>
2024-01-15 23:44:11 +0900

    [update] コメント変更

--- app/Http/Controllers/HomeModeController.php
+++ app/Http/Controllers/HomeModeController.php
@@ -199,7 +199,7 @@ class HomeModeController extends Controller
             return redirect()->route('home')->with('flash_error_message', 'ファイルを選択してください');
         }
         $fileService = new FileService('public/import/');
-        // テキストファイル以外はエラーを表示
+        // 20MB以上のテキストファイルでなければエラーを表示
         if (!$fileService->upload($files)) {
             return redirect()->route('home')->with('flash_error_message', '20MB未満の.txtファイルのみ可');
         }
