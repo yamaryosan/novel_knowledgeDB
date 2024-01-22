78bc34573f33fc3d283fade8c07ff4f42ab772bd
yamaryosan <canda3267@gmail.com>
2024-01-22 23:02:05 +0900

    [fix] トークンによって多重を防止

--- app/Jobs/WriteTriviaToFileJob.php
+++ app/Jobs/WriteTriviaToFileJob.php
@@ -18,11 +18,13 @@ class WriteTriviaToFileJob implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public $timeout = 1800;
+    protected $token;
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct($token)
     {
+        $this->token = $token;
     }
 
     /**
@@ -30,6 +32,7 @@ class WriteTriviaToFileJob implements ShouldQueue
      */
     public function handle(): void
     {
+        Cache::add('progress', 0, 1800);
         // ファイル名を取得
         $filename = date('YmdHis') . '.txt';
         // ファイルパスを取得
@@ -39,14 +42,20 @@ class WriteTriviaToFileJob implements ShouldQueue
         $total = count($trivia);
         $count = 0;
 
+        if (count($trivia) === 0) {
+            Cache::forget("exporting:{$this->token}");
+        }
+
+        $all_sentence = '';
         foreach ($trivia as $trivium) {
-            Storage::append($path, '【タイトル】' . $trivium->title);
-            Storage::append($path, '【総論】' . $trivium->summary);
-            Storage::append($path, '【本文】' . $trivium->detail);
+            $all_sentence = '【タイトル】' . $trivium->title . PHP_EOL . '【総論】' . $trivium->summary . PHP_EOL . '【本文】' . $trivium->detail . PHP_EOL;
+            Storage::append($path, '【タイトル】' . $all_sentence);
             $count++;
             // 進捗を表示
             $progress = round($count / $total * 100);
             Cache::put('progress', $progress, 1800);
         }
+        // ジョブが完了したらトークンをキャッシュから削除
+        Cache::forget("exporting:{$this->token}");
     }
 }
