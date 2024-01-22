d954393331907399eb51185cd7b3f06fce8265a0
yamaryosan <canda3267@gmail.com>
2024-01-22 23:03:33 +0900

    [update] 関数を使用して記述を削減

--- resources/js/home_mode/export.js
+++ resources/js/home_mode/export.js
@@ -1,10 +1,8 @@
-const exportButton = document.querySelector('.export');
+const button = document.querySelector('.export');
 
-exportButton.addEventListener('click', (event)=> {
-    // プログレスバーを表示
-    const progressBar = document.getElementById('progressBar');
+button.addEventListener('click', (event)=> {
     // エクスポートするか確認
-    const link = exportButton.dataset.export;
+    const link = button.dataset.export;
     if (window.confirm('エクスポートしますか？') === true) {
         window.location.href = link;
     } else {
@@ -12,19 +10,25 @@ exportButton.addEventListener('click', (event)=> {
     }
 });
 
+// 進捗中のみプログレスバーを表示する
+const progressBar = document.getElementById('progressBar');
+
+function showProgressBar () {
+    if (progressBar.value === 0 || progressBar.value === 100) {
+        progressBar.style.display = 'none';
+        return false;
+    }
+}
+
 // 一定間隔で進捗をチェック
-setInterval(updateProgress, 10);
+setInterval(updateProgress, 1000);
 
 function updateProgress() {
     const exportButton = document.querySelector('.export');
     const progressBar = document.getElementById('progressBar');
-    // 進捗が100%の時はプログレスバーを非表示
-    if (progressBar.value === 100) {
-        progressBar.style.display = 'none';
-        return false;
-    } else {
-        progressBar.style.display = 'block';
-    }
+    const lastRow = document.querySelectorAll('table tbody tr:last-child')[0];
+    const lastButtons = document.querySelectorAll('table tbody tr:last-child a');
+
     // 進捗を取得
     fetch('/progress')
         .then(response => {
@@ -34,49 +38,18 @@ function updateProgress() {
             return response.json();
         })
         .then(data => {
+            // 進捗を取得
             const progress = data;
+            console.log(progress);
+            // 進捗が0%でも100%でもない場合はプログレスバーを表示し、ボタンとテーブル行を無効化
+            if (progress > 0 && progress < 100) {
+                progressBar.style.display = 'block';
+                disableButton(exportButton);
+                disableRow(lastRow);
+                disableLastRowButton(lastButtons);
+            }
             // プログレスバーの更新 (プログレスバーはprogress要素で実装)
             progressBar.value = progress;
-            // ボタンを無効化
-            exportButton.disabled = true;
-            exportButton.style.backgroundColor = '#ccc';
-            exportButton.style.boxShadow = 'none';
-            exportButton.style.top = '0';
-            exportButton.style.cursor = 'not-allowed';
-            // テーブル最終行を無効化
-            const lastRow = document.querySelectorAll('table tbody tr:last-child');
-            lastRow[0].style.backgroundColor = '#666';
-            lastRow[0].style.color = '#fff';
-            lastRow[0].style.fontWeight = 'bold';
-            lastRow[0].style.textShadow = 'none';
-            // テーブル最終行のボタンを無効化
-            const lastButtons = document.querySelectorAll('table tbody tr:last-child a');
-            lastButtons.forEach((lastButton) => {
-                lastButton.style.color = '#fff';
-                lastButton.style.textShadow = 'none';
-                lastButton.style.pointerEvents = 'none';
-            });
-            // プログレスバーの値が100%になったらボタンを有効化
-            if (progressBar.value === 100) {
-                exportButton.disabled = false;
-                exportButton.style.backgroundColor = '#ade8e6';
-                exportButton.style.boxShadow = '0 0 10px #999';
-                exportButton.style.top = '6px';
-            }
-            // プログレスバーの値が100%になったらテーブル最終行を有効化
-            if (progressBar.value === 100) {
-                lastRow[0].style.backgroundColor = '#fff';
-                lastRow[0].style.color = '#666';
-                lastRow[0].style.fontWeight = 'normal';
-            }
-            // プログレスバーの値が100%になったらテーブル最終行のボタンを有効化
-            if (progressBar.value === 100) {
-                lastButtons.forEach((lastButton) => {
-                    lastButton.style.color = '#666';
-                    lastButton.style.cursor = 'pointer';
-                    lastButton.style.pointerEvents = 'auto';
-                });
-            }
         })
         .catch(error => {
             console.error('Fetch error:', error);
@@ -121,15 +94,19 @@ function enableRow (row) {
 }
 
 // テーブル最終行のa要素を無効化
-function disableLastRowButton (lastButton) {
-    lastButton.style.color = '#fff';
-    lastButton.style.textShadow = 'none';
-    lastButton.style.pointerEvents = 'none';
+function disableLastRowButton (lastButtons) {
+    lastButtons.forEach(lastButton => {
+        lastButton.style.color = '#fff';
+        lastButton.style.textShadow = 'none';
+        lastButton.style.pointerEvents = 'none';
+    });
 }
 
 // テーブル最終行のa要素を有効化
-function enableLastRowButton (lastButton) {
-    lastButton.style.color = '#666';
-    lastButton.style.cursor = 'pointer';
-    lastButton.style.pointerEvents = 'auto';
+function enableLastRowButton (lastButtons) {
+    lastButtons.forEach(lastButton => {
+        lastButton.style.color = '#fff';
+        lastButton.style.textShadow = 'none';
+        lastButton.style.pointerEvents = 'none';
+    });
 }
