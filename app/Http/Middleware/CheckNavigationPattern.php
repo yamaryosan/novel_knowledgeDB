37cab875bc99b57c743cbe46fd55aa2bb21112bf
yamaryo <canda3267@gmail.com>
2024-02-01 20:25:26 +0900

    [update] 環境変数を使うように変更

--- app/Http/Middleware/CheckNavigationPattern.php
+++ app/Http/Middleware/CheckNavigationPattern.php
@@ -8,9 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CheckNavigationPattern
 {
-    // 自宅閲覧モードに遷移できる特定のパターン
-    const ROUTE_PATTERN = ["recommend", "recommend", "form"];
-
     /**
      * Handle an incoming request.
      *
@@ -33,7 +30,7 @@ class CheckNavigationPattern
     // 履歴の最後が特定のパターンに一致するかチェック
     protected function isValidPattern(array $history)
     {
-        $pattern = self::ROUTE_PATTERN;
+        $pattern = explode(',', env('ROUTE_PATTERN'));
         if (count($history) < count($pattern)) {
             return false;
         }
